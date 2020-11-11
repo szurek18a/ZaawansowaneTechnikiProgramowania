90dff8f4d30503b60b24efa01ddcb6927b300072
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2020-11-11 17:22:32 +0100

    Zad 496 got + segregacja

--- Lab1/Zad438/Zad438.cpp
+++ Lab1/Zad438/Zad438.cpp
@@ -1,20 +1,38 @@
 ﻿// Zad438.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 
-#include <iostream>
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+bool czy_pierwsza(int n)
+{
+	if (n < 2)
+		return false;
+
+	for (int i = 2; i * i <= n; i++)
+		if (n % i == 0)
+			return false;
+	return true;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n, liczba;
+
+	cout << "Podaj liczbe testow: ";
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> liczba;
+
+		if (czy_pierwsza(liczba))
+			cout << "Liczba " << liczba << " jest pierwsza" << endl;
+		else
+			cout << "Liczba " << liczba << " nie jest pierwsza" << endl;
+	}
+
+	return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ Lab1/Zad496/Zad496.cpp
@@ -0,0 +1,28 @@
+﻿// Zad496.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int silnia(int n)
+{
+    if (n == 0) return 1;
+    else return n * silnia(n - 1);
+}
+int D, n;
+int main()
+{
+    cin >> D;
+    for (int i = 0; i < D; i++)
+    {
+        cin >> n;
+        if (n < 10)
+        {
+            cout << silnia(n) % 100 / 10 << " " << silnia(n) % 10 << endl;
+        }
+        else cout << "0 0" << endl;
+    }
+    return 0;
+}
+
