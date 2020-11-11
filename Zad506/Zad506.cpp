918797a882139a29ca5f54b2429eda6ad789afbe
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2020-11-11 16:22:25 +0100

    Zad506 got

---
+++ Zad506/Zad506.cpp
@@ -0,0 +1,36 @@
+﻿// Zad506.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int C;
+    cin >> C;
+    while (C--)
+    {
+        string tekst;
+        cin >> tekst;
+        int licznik = 1;
+        for (int i = 1; i <= tekst.length(); i++)
+        {
+            if (tekst[i] != tekst[i - 1])
+            {
+                if (licznik > 2)
+                    cout << tekst[i - 1] << licznik;
+                else if (licznik == 2)
+                    cout << tekst[i - 1] << tekst[i - 1];
+                else
+                    cout << tekst[i - 1];
+                licznik = 1;
+            }
+            else
+                licznik++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
