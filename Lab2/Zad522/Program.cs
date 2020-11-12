a09c1b7e2c3bcdfb2203b00eb0c56aef0e75658a
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2020-11-12 15:02:47 +0100

    Zad522 got

---
+++ Lab2/Zad522/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for(int i=0; i < N; i++)
+            {
+                string tekst = Console.ReadLine();
+                string[] tekstTab = tekst.Split(' ');
+                int mniejsza = int.Parse(tekstTab[0]);
+                int wieksza = int.Parse(tekstTab[1]);
+                int tmp;
+                if(mniejsza > wieksza)
+                {
+                    tmp = mniejsza;
+                    mniejsza = wieksza;
+                    wieksza = tmp;
+                }
+                int wynik = wieksza;
+                while (wynik%mniejsza != 0 || wynik % wieksza != 0)
+                {
+                    wynik += wieksza;
+                }
+                Console.WriteLine(wynik);
+            }
+        }
+    }
+}
