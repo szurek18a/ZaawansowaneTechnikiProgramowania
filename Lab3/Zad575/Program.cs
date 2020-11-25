ad51ab71cb8688f98a038a3e5ddf5a64c41dd36d
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2020-11-25 15:31:22 +0100

    Kolejne zadania

---
+++ Lab3/Zad575/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad575
+{
+    class Program
+    {
+        static bool CzyZnaczaca(int liczba)
+        {
+            int suma = 0;
+            int licznik = 0;
+
+            for(int i=2; i<Math.Sqrt(liczba); i++)
+            {
+                if(liczba%i == 0)
+                {
+                    suma += i;
+                    licznik++;
+                }
+            }
+
+            double srednia = (suma * 1.0) / licznik;
+            if (srednia <= Math.Sqrt(liczba))
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            while(t > 0)
+            {
+                string[] liczby = Console.ReadLine().Split(' ');
+                int a = int.Parse(liczby[0]);
+                int b = int.Parse(liczby[1]);
+
+                int licznik = 0;
+                for(int i=a; i<=b; i++)
+                {
+                    if (CzyZnaczaca(i))
+                        licznik++;
+                }
+                Console.WriteLine(licznik);
+                t--;
+            }
+        }
+    }
+}
---
+++ Lab3/Zad723/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                Queue<string> kol = new Queue<string>(Console.ReadLine().Split(' '));
+                kol.Dequeue();
+                var item = kol.Dequeue();
+                kol.Enqueue(item);
+
+                foreach (var x in kol)
+                {
+                    Console.Write(x + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
