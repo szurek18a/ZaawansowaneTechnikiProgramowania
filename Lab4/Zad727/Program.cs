70c78480cb8c548009b2a0e301ef73b84db5acef
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2020-12-08 17:17:45 +0100

    Zad727 got

--- Lab4/Zad727/Program.cs
+++ Lab4/Zad727/Program.cs
@@ -10,7 +10,24 @@ namespace Zad727
     {
         static void Main(string[] args)
         {
+            int n, m;
+            n = int.Parse(Console.ReadLine());
+            string str = null;
+            for(int i=0; i<n; i++)
+            {
+                m = int.Parse(Console.ReadLine());
+                spare(str, m, 1);
+            }           
+        }
 
+        static void spare(string s, int l, int min)
+        {
+            for(int i=min; i <= l/2; i++)
+            {
+                spare(s + i.ToString()+" ", l-i, i);
+            }
+            s = s + l.ToString();
+            Console.WriteLine(s);
         }
     }
 }
