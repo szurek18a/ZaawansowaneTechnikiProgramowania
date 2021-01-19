f898aae3b2055d3cab9c3a666d4f05a1d3781bdb
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-19 14:44:26 +0100

    Zad1261 got

---
+++ Odrabianie/Zad1261/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1261
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t, pesel_wynik;
+            char[] pesel;
+            t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                String str = Console.ReadLine();
+                pesel = str.ToCharArray();
+                pesel_wynik = (pesel[0] - '0') * 1 + (pesel[1] - '0') * 3 + (pesel[2] - '0') * 7 + (pesel[3] - '0') * 9 + (pesel[4] - '0') * 1 +
+                (pesel[5] - '0') * 3 + (pesel[6] - '0') * 7 + (pesel[7] - '0') * 9 + (pesel[8] - '0') * 1 + (pesel[9] - '0') * 3 + (pesel[10] - '0') * 1;
+                if (pesel_wynik > 0)
+                {
+                    if (pesel_wynik % 10 == 0)
+                    {
+                        Console.WriteLine("D\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("N\n");
+                    }
+                }
+            }
+        }
+    }
+}
