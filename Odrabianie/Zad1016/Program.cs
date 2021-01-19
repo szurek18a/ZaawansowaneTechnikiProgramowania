c16b12d6097e4b731a5767fd9a7096d9e5ec4403
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-19 19:06:50 +0100

    got kolejne

---
+++ Odrabianie/Zad1016/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			{
+				int t;
+				t = int.Parse(Console.ReadLine());
+				for (int i = 0; i < t; ++i)
+				{
+					int v1, v2;
+					string[] text = Console.ReadLine().Split();
+					v1 = int.Parse(text[0]);
+					v2 = int.Parse(text[1]);
+					Console.WriteLine(2 * (v1 * v2) / (v1 + v2));
+				}
+			}
+		}
+    }
+}
