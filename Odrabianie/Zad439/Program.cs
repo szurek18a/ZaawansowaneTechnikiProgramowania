448fc4c7d106f92c77edd45d82ff26c8351d8802
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-20 18:09:55 +0100

    zad439 nie przechodzi spoj

---
+++ Odrabianie/Zad439/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad439
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{							
+				string[] text = Console.ReadLine().Split();
+				var l1 = BigInteger.Parse(text[0]);
+				var l2 = BigInteger.Parse(text[1]);
+
+				var wynik = BigInteger.Multiply(l1, l2);
+
+				Console.WriteLine(wynik.ToString());
+			}		
+		}
+    }
+}
