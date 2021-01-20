60448c3618b27075fefe153293ed6e26ff542678
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-20 16:00:08 +0100

    pierwsze srednie gotowe

---
+++ Odrabianie/Zad1293/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {			
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				BigInteger a, b;
+				string[] text = Console.ReadLine().Split();
+				a = BigInteger.Parse(text[0]);
+				b = BigInteger.Parse(text[1]);
+
+				Console.WriteLine(a + b);
+			}			
+		}
+    }
+}
