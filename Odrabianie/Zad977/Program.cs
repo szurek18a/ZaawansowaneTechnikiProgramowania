64294db0c4ac013aaa8e9ddac428874123ef1a85
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-27 20:20:01 +0100

    łatwe gotowe

---
+++ Odrabianie/Zad977/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad977
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tablica = Console.ReadLine().Split(' ');
+            List<string> lista = new List<string>(tablica);
+            lista.Reverse();
+            foreach (string s in lista)
+            {
+                Console.Write(s + ' ');
+            }
+        }
+    }
+}
