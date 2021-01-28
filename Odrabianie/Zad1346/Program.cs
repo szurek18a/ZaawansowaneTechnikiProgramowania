d3e14ab0cbdb690e78de4b4a5ad9374357fc67cf
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-28 11:49:24 +0100

    średnie

---
+++ Odrabianie/Zad1346/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1346
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string wejscie;
+            while (!String.IsNullOrWhiteSpace(wejscie = Console.ReadLine()))
+            {
+                string haslo = "";
+
+                for (int j = 0; j < 20; j += 2)
+                {
+                    int tmp = ((int)wejscie[j] - 65) + ((int)wejscie[j + 1] - 65) * 16;
+
+                    haslo += (char)tmp;
+                }
+
+                Console.WriteLine(haslo);
+
+            }
+            }
+    }
+}
