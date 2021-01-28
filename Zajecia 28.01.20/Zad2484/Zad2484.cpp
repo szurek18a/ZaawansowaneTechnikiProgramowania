98212aa42ba8242060b83074069e81205647656d
Kamil Szurgot <74003158+szurek18a@users.noreply.github.com>
2021-01-28 15:11:59 +0100

    srednie

---
+++ Zajecia 28.01.20/Zad2484/Zad2484.cpp
@@ -0,0 +1,15 @@
+﻿// Zad2484.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main(int a, int b)
+{
+	if (std::cin >> a)
+	{
+		if (std::cin >> b)
+		{
+			if (std::cout << a + b) {}
+		}
+	}
+}
